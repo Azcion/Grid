f99f4d5209bed0f47a7ee15c5fcd64669944fa39
Azcion <zak.klajderic@gmail.com>
2016-09-29 00:12:24 +0200

    Refactor sprite processing

--- Assets/Scripts/TileSprite.cs
+++ Assets/Scripts/TileSprite.cs
@@ -1,28 +1,38 @@
-﻿using UnityEngine;
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using UnityEngine;
 
 namespace Assets.Scripts {
 
-	public static class TileSprite {
+	[SuppressMessage ("ReSharper", "MemberCanBePrivate.Global")]
+	[SuppressMessage ("ReSharper", "UnassignedField.Global")]
+	public class TileSprite : MonoBehaviour {
 
-		public static readonly Sprite NONE;
-		public static readonly Sprite WATER;
-		public static readonly Sprite SAND;
-		public static readonly Sprite GRASS;
-		public static readonly Sprite PLANT;
-		public static readonly Sprite MOUNTAIN;
-		public static readonly Sprite SNOW;
+		public static Sprite None;
+		public static Sprite Water;
+		public static Sprite Sand;
+		public static Sprite Grass;
+		public static Sprite Plant;
+		public static Sprite Mountain;
+		public static Sprite Snow;
 
-		static TileSprite () {
-			Rect r = new Rect(0, 0, 32, 32);
-			Vector2 p = Vector2.zero;
+		public Sprite NoneSprite;
+		public Sprite WaterSprite;
+		public Sprite SandSprite;
+		public Sprite GrassSprite;
+		public Sprite PlantSprite;
+		public Sprite MountainSprite;
+		public Sprite SnowSprite;
 
-			NONE = Sprite.Create(Resources.Load("None") as Texture2D, r, p);
-			WATER = Sprite.Create(Resources.Load("Water") as Texture2D, r, p);
-			SAND = Sprite.Create(Resources.Load("Sand") as Texture2D, r, p);
-			GRASS = Sprite.Create(Resources.Load("Grass") as Texture2D, r, p);
-			PLANT = Sprite.Create(Resources.Load("Plant") as Texture2D, r, p);
-			MOUNTAIN = Sprite.Create(Resources.Load("Mountain") as Texture2D, r, p);
-			SNOW = Sprite.Create(Resources.Load("Snow") as Texture2D, r, p);
+		[UsedImplicitly]
+		private void Start () {
+			None = NoneSprite;
+			Water = WaterSprite;
+			Sand = SandSprite;
+			Grass = GrassSprite;
+			Plant = PlantSprite;
+			Mountain = MountainSprite;
+			Snow = SnowSprite;
 		}
 
 	}
