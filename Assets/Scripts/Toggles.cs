3ac3530d34c2a6ab3e7d9e7037b14121014af532
Azcion <zak.klajderic@gmail.com>
2016-09-29 00:13:32 +0200

    Add day/night cycle toggle

---
+++ Assets/Scripts/Toggles.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.Scripts {
+
+	public class Toggles : MonoBehaviour {
+
+		public static bool DoCycle = true;
+
+		[UsedImplicitly]
+		public void ToggleCycle (bool flag) {
+			DoCycle = flag;
+		}
+
+	}
+
+}
