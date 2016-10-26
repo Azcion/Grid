5803496e973db478f3e7e2b64682ef7878faf8cf
Azcion <zak.klajderic@gmail.com>
2016-10-26 05:25:27 +0200

    Add implicit tag to Flora

--- Assets/Scripts/Flora.cs
+++ Assets/Scripts/Flora.cs
@@ -1,8 +1,10 @@
 ﻿using Assets.Scripts.Enums;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Assets.Scripts {
 
+	[UsedImplicitly]
 	public class Flora : MonoBehaviour {
 
 		public FloraType Type;
