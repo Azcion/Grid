2a9caf67825dc3b888ed037ad1ed9fd56c133af0
Azcion <Azcion@users.noreply.github.com>
2018-08-20 23:35:55 +0200

    Make Human inherit from Thing

---
+++ Assets/Scripts/Things/Thing.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Main;
+using Assets.Scripts.Utils;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.Scripts.Things {
+
+	[RequireComponent(typeof(SpriteRenderer), typeof(BoxCollider2D))]
+	public class Thing : MonoBehaviour {
+
+		[UsedImplicitly]
+		private void OnEnable () {
+			Vector2 v = transform.localPosition;
+			transform.localPosition = new Vector3(v.x, v.y, Order.THING);
+		}
+
+		[UsedImplicitly]
+		private void OnMouseDown () {
+			Selection.Select(transform);
+		}
+
+	}
+
+}
