d637b17e1d73ee4017848f1d745e2847bfb2502f
Azcion <Azcion@users.noreply.github.com>
2018-08-22 05:38:01 +0200

    Add AssetLoader

---
+++ Assets/Scripts/Graphics/AssetLoader.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.Scripts.Graphics {
+
+	public class AssetLoader : MonoBehaviour {
+
+		public static Material DiffuseMat;
+
+		[UsedImplicitly]
+		private void Start () {
+			DiffuseMat = Resources.Load<Material>("Materials/Diffuse");
+		}
+
+	}
+
+}
