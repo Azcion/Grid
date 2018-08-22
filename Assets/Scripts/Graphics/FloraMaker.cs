1aa90900f0d01b09f989b4d014e9e789e72be1a0
Azcion <Azcion@users.noreply.github.com>
2018-08-22 05:42:04 +0200

    Simplify Flora container

--- Assets/Scripts/Graphics/FloraMaker.cs
+++ Assets/Scripts/Graphics/FloraMaker.cs
@@ -16,10 +16,7 @@ namespace Assets.Scripts.Graphics {
 		public GameObject ChunkContainer;
 
 		[UsedImplicitly]
-		public GameObject PlantContainer;
-
-		[UsedImplicitly]
-		public GameObject TreeContainer;
+		public GameObject Container;
 		#endregion
 
 		[UsedImplicitly]
@@ -41,10 +38,8 @@ namespace Assets.Scripts.Graphics {
 
 		private void Initialize (Transform t) {
 			float scale = (float) System.Math.Round(Random.Range(.5f, 1), 2);
-			Vector3 vScale = new Vector3(Random.value > .5 ? scale : -scale, scale, 1);
 			Vector3 position = new Vector3(t.position.x, t.position.y, Order.THING);
 
-			Transform container;
 			FloraType type;
 
 			switch (t.GetComponent<Tile>().Type) {
@@ -53,7 +48,6 @@ namespace Assets.Scripts.Graphics {
 						return;
 					}
 
-					container = PlantContainer.transform;
 					type = FloraType.Cactus;
 					break;
 				case TileType.Grass:
@@ -62,12 +56,10 @@ namespace Assets.Scripts.Graphics {
 					}
 
 					if (Random.value < .50) {
-						container = PlantContainer.transform;
 						type = FloraType.Grass;
 						break;
 					}
 
-					container = TreeContainer.transform;
 					type = FloraType.Palm;
 					break;
 				case TileType.Dirt:
@@ -75,7 +67,6 @@ namespace Assets.Scripts.Graphics {
 						return;
 					}
 
-					container = PlantContainer.transform;
 					type = FloraType.Agave;
 					break;
 				default:
