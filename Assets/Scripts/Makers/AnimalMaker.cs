c7800968d38f414e468544aa984f2025edd124fa
Azcion <Azcion@users.noreply.github.com>
2019-08-20 02:07:58 +0200

    Generate groups of animals

--- Assets/Scripts/Makers/AnimalMaker.cs
+++ Assets/Scripts/Makers/AnimalMaker.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Assets.Scripts.Defs;
 using Assets.Scripts.Enums;
 using Assets.Scripts.Things;
+using Assets.Scripts.Utils;
 using JetBrains.Annotations;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -15,9 +19,24 @@ namespace Assets.Scripts.Makers {
 		[UsedImplicitly, SerializeField] private GameObject _chunkContainer = null;
 		[UsedImplicitly, SerializeField] private GameObject _container = null;
 
+		private static readonly ulong ValidTilesMask;
+
 		private static readonly List<TileType> ValidTiles = new List<TileType> {
 			TileType.Mossy, TileType.Sand, TileType.Soil, TileType.SoilRich, TileType.Gravel, TileType.PackedDirt, TileType.Ice
 		};
+
+		static AnimalMaker () {
+			ValidTilesMask = 0;
+
+			foreach (TileType type in ValidTiles) {
+				ulong mask = (ulong) (1 << (int) type);
+				ValidTilesMask |= mask;
+			}
+		}
+
+		private static bool IsValid (TileType type) {
+			return ((ulong) (1 << (int) type) & ValidTilesMask) > 0;
+		}
 
 		[UsedImplicitly]
 		private void Start () {
@@ -38,24 +57,53 @@ namespace Assets.Scripts.Makers {
 		private void Populate (GameObject prefab) {
 			foreach (Transform c in _chunkContainer.transform) {
 				foreach (Transform t in c) {
-					Initialize(prefab, t);
+					if (Random.value < .998f) {
+						continue;
+					}
+
+					if (!IsValid(t.GetComponent<Tile>().Type)) {
+						continue;
+					}
+
+					AnimalDef def = DefLoader.GetRandomAnimalDef();
+					int x = (int) t.transform.position.x;
+					int y = (int) t.transform.position.y;
+
+					if (def.Solitary) {
+						Initialize(prefab, def, x, y);
+					} else {
+						InitializeGroup(prefab, def, x, y);
+					}
 				}
 			}
 		}
 
-		private void Initialize (GameObject prefab, Transform t) {
-			if (Random.value < .998f) {
-				return;
-			}
+		private void InitializeGroup (GameObject prefab, AnimalDef def, int x, int y) {
+			int groupSize = Random.Range(1, 4);
+			float rv = Random.value;
+			groupSize += rv > .5f ? 1 : 0;
+			groupSize += rv > .8f ? 1 : 0;
+			Vector2Int v0 = Calc.Clamp(new Vector2Int(x - 5, y - 5));
+			Vector2Int v1 = Calc.Clamp(new Vector2Int(x + 6, y + 6));
+			List<Vector2Int> usedPoints = new List<Vector2Int>();
+			
 
-			if (ValidTiles.Contains(t.GetComponent<Tile>().Type) == false) {
-				return;
-			}
+			for (int i = 0; i < groupSize; i++) {
+				Vector2Int v = new Vector2Int(Random.Range(v0.x, v1.x), Random.Range(v0.y, v1.y));
+				
+				if (usedPoints.Contains(v)) {
+					continue;
+				}
 
-			//todo entry point for animal selection
-			AnimalDef def = DefLoader.GetRandomAnimalDef();
-			int x = (int) t.position.x;
-			int y = (int) t.position.y;
+				usedPoints.Add(v);
+
+				if (IsValid(TileMaker.GetTile(v.x, v.y).Type)) {
+					Initialize(prefab, def, v.x, v.y);
+				}
+			}
+		}
+
+		private void Initialize (GameObject prefab, AnimalDef def, int x, int y) {
 			Vector3 pos = new Vector3(x, y, Order.ANIMAL);
 			GameObject go = Instantiate(prefab, pos, Quaternion.identity, _container.transform);
 			go.name = def.DefName;
--- Assets/Scripts/Utils/Calc.cs
+++ Assets/Scripts/Utils/Calc.cs
@@ -13,6 +13,11 @@ namespace Assets.Scripts.Utils {
 			return Clamp(v, 0, Map.YTiles - 1, 0, Map.YTiles - 1);
 		}
 
+		public static Vector2Int Clamp (Vector2Int v) {
+			Vector2 v2 = Clamp(new Vector2(v.x, v.y));
+			return new Vector2Int((int) v2.x, (int) v2.y);
+		}
+
 		private static Vector2 Clamp (Vector2 v, int x0, int x1, int y0, int y1) {
 			int x = (int) v.x;
 			int y = (int) v.y;
