5b1a9e01507aec5f54af8edfbd14911b0ac34a80
Azcion <Azcion@users.noreply.github.com>
2019-08-20 02:07:13 +0200

    Add Solitary tag to defs

--- Assets/Scripts/Defs/AnimalDef.cs
+++ Assets/Scripts/Defs/AnimalDef.cs
@@ -1,11 +1,12 @@
-﻿using System.Xml.Serialization;
+﻿using System.ComponentModel;
+using System.Xml.Serialization;
 
 namespace Assets.Scripts.Defs {
 
 	[XmlType("AnimalDef")]
 	public class AnimalDef : ThingDef, IThingDef {
 
-
+		[DefaultValue(true)] public bool Solitary;
 
 	}
 
