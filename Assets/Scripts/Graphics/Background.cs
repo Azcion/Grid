115b720995a4d0feb2eca9a5b57ae2462f7e0205
Azcion <Azcion@users.noreply.github.com>
2020-06-01 20:53:32 +0200

    Add background material

--- Assets/Scripts/Graphics/Background.cs
+++ Assets/Scripts/Graphics/Background.cs
@@ -10,7 +10,6 @@ namespace Assets.Scripts.Graphics {
 			transform.position = new Vector3(Map.YHalf, Map.YHalf, Order.BACKGROUND);
 			transform.localScale = new Vector3(Map.YTiles + 1, Map.YTiles + 1);
 			MeshRenderer mr = GetComponent<MeshRenderer>();
-			mr.material.color = new Color32(35, 35, 35, 255);
 			mr.enabled = true;
 		}
 
