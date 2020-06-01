55d2439719b1075d3bfc75fcc643799e4272de2e
Azcion <Azcion@users.noreply.github.com>
2020-06-01 22:26:34 +0200

    Add weather toggle

--- Assets/Scripts/Graphics/Weather.cs
+++ Assets/Scripts/Graphics/Weather.cs
@@ -5,12 +5,18 @@ namespace Assets.Scripts.Graphics {
 
 	public class Weather : MonoBehaviour {
 
+		private static MeshRenderer _renderer;
+
+		public static void SetActive (bool flag) {
+			_renderer.enabled = flag;
+		}
+
 		[UsedImplicitly]
 		private void Start () {
 			transform.position = new Vector3(Map.YHalf, Map.YHalf, Order.WEATHER);
 			transform.localScale = new Vector3(Map.YTiles, Map.YTiles);
-			MeshRenderer mr = GetComponent<MeshRenderer>();
-			mr.enabled = true;
+			_renderer = GetComponent<MeshRenderer>();
+			_renderer.enabled = true;
 		}
 		
 	}
--- Assets/Scripts/Main/Toggles.cs
+++ Assets/Scripts/Main/Toggles.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using Assets.Scripts.Graphics;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Assets.Scripts.Main {
@@ -7,9 +8,17 @@ namespace Assets.Scripts.Main {
 
 		public static bool DoCycle;
 
+		private static bool _showWeather = true;
+
 		[UsedImplicitly]
 		public void ToggleCycle () {
 			DoCycle = !DoCycle;
+		}
+
+		[UsedImplicitly]
+		public void ToggleWeather () {
+			_showWeather = !_showWeather;
+			Weather.SetActive(_showWeather);
 		}
 
 	}
