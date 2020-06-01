8ddaa258d680edf252d825c7fb288b91c2907a94
Azcion <Azcion@users.noreply.github.com>
2020-06-02 00:58:06 +0200

    Add categories to architect

--- Assets/Scripts/UI/ArchitectController.cs
+++ Assets/Scripts/UI/ArchitectController.cs
@@ -5,15 +5,43 @@ namespace Assets.Scripts.UI {
 
 	public class ArchitectController : MonoBehaviour {
 
-		[UsedImplicitly, SerializeField] private GameObject _architect = null;
+		private GameObject _architect;
+		private GameObject _structure;
+		private GameObject _production;
+		private GameObject _activeCategory;
 
 		[UsedImplicitly]
 		public void Toggle () {
-			if (_architect == null) {
+			_architect.SetActive(!_architect.activeSelf);
+		}
+
+		[UsedImplicitly]
+		public void ToggleStructure () {
+			ToggleActive();
+			_activeCategory = _structure;
+			ToggleActive();
+		}
+
+		[UsedImplicitly]
+		public void ToggleProduction () {
+			ToggleActive();
+			_activeCategory = _production;
+			ToggleActive();
+		}
+
+		private void ToggleActive () {
+			if (_activeCategory == null) {
 				return;
 			}
 
-			_architect.SetActive(!_architect.activeSelf);
+			_activeCategory.SetActive(!_activeCategory.activeSelf);
+		}
+
+		[UsedImplicitly]
+		private void Start () {
+			_architect = transform.Find("Container").gameObject;
+			_structure = _architect.transform.Find("Structure").GetChild(1).gameObject;
+			_production = _architect.transform.Find("Production").GetChild(1).gameObject;
 		}
 		
 	}
---
+++ Assets/Scripts/UI/ArchitectDescriptionController.cs
@@ -0,0 +1,36 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI {
+
+	public class ArchitectDescriptionController : MonoBehaviour {
+
+		private static GameObject _container;
+		private static Text _name;
+		private static Text _description;
+
+		public static void Set (string label, string description) {
+			_name.text = label;
+			_description.text = description;
+		}
+
+		public static void Show () {
+			_container.SetActive(true);
+		}
+
+		public static void Hide () {
+			_container.SetActive(false);
+		}
+
+		[UsedImplicitly]
+		private void Start () {
+			_container = transform.GetChild(0).gameObject;
+			Text[] components = _container.GetComponentsInChildren<Text>();
+			_name = components[0];
+			_description = components[1];
+		}
+
+	}
+
+}
